f42d7c2f25a11d2a487d3911c8dbd587c1ea559d
Migs <miguelbaliog@gmail.com>
2021-08-18 20:12:10 +0800

    Second commit

---
+++ app/src/main/java/ph/edu/ph/mobdeve/s18/baliog/miguel/countit/LogMealActivity.java
@@ -0,0 +1,14 @@
+package ph.edu.ph.mobdeve.s18.baliog.miguel.countit;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LogMealActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_meal);
+    }
+}
